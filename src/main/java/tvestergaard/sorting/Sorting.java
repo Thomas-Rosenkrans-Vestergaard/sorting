1bfd903d806fd537119e494edb0bcebafd142bd3
Thomas <Thomas-Rosenkrans-Vestergaard@users.noreply.github.com>
2018-02-15 15:19:25 +0100

    quicksort and O(n logn) memory usage mergesort

--- src/main/java/tvestergaard/sorting/Sorting.java
+++ src/main/java/tvestergaard/sorting/Sorting.java
@@ -1,7 +1,120 @@
 package tvestergaard.sorting;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class Sorting
 {
+
+	/**
+	 * Sorts the provided array using the merge-sort algorithm. The merge sort algorithm is stable meaning that equal
+	 * values in the array retain their order when sorted.
+	 *
+	 * @param array      The array to be sorted.
+	 * @param comparator The comparator that dictates the sorting of the array.
+	 * @param <T>        The type of value to sort.
+	 */
+	public static <T> void mergesort(T[] array, Comparator<T> comparator)
+	{
+
+		if (array.length < 2)
+			return;
+
+		int mid   = array.length / 2;
+		T[] left  = (T[]) new Object[mid];
+		T[] right = (T[]) new Object[array.length - mid];
+
+		int index = 0;
+		for (int x = 0; x < left.length; x++)
+			left[x] = array[index++];
+
+		for (int x = 0; x < right.length; x++)
+			right[x] = array[index++];
+
+		mergesort(left, comparator);
+		mergesort(right, comparator);
+		merge(array, left, right, comparator);
+	}
+
+	/**
+	 * Merges the sorted array <code>left</code> and <code>right</code> into <code>array</code>. The arrays are
+	 * merged in <code>O(n)</code> time.
+	 *
+	 * @param array      The array to merge <code>left</code> and <code>right</code> into.
+	 * @param left       The first array to merge from.
+	 * @param right      The second array to merge from.
+	 * @param comparator The comparator to use when merging.
+	 * @param <T>        The type of the values in the arrays.
+	 */
+	private static <T> void merge(T[] array, T[] left, T[] right, Comparator<T> comparator)
+	{
+		int l = 0;
+		int r = 0;
+		for (int x = 0; x < array.length; x++) {
+			if (l == left.length)
+				array[x] = right[r++];
+			else if (r == right.length)
+				array[x] = left[l++];
+			else
+				array[x] = comparator.compare(left[l], right[r]) < 1 ? left[l++] : right[r++];
+		}
+	}
+
+	/**
+	 * Sorts the provided array using the merge-sort algorithm. The merge sort algorithm is stable meaning that equal
+	 * values in the array retain their order when sorted.
+	 *
+	 * @param list       The list to be sorted.
+	 * @param comparator The comparator that dictates the sorting of the list.
+	 * @param <T>        The type of value to sort.
+	 */
+	public static <T> void mergesort(List<T> list, Comparator<T> comparator)
+	{
+		int size = list.size();
+
+		if (size < 2)
+			return;
+
+		int mid   = size / 2;
+		T[] left  = (T[]) new Object[mid];
+		T[] right = (T[]) new Object[size - mid];
+
+		int index = 0;
+		for (int x = 0; x < left.length; x++)
+			left[x] = list.get(index++);
+
+		for (int x = 0; x < right.length; x++)
+			right[x] = list.get(index++);
+
+		mergesort(left, comparator);
+		mergesort(right, comparator);
+		merge(list, left, right, comparator);
+	}
+
+	/**
+	 * Merges the sorted array <code>left</code> and <code>right</code> into <code>array</code>. The arrays are
+	 * merged in <code>O(n)</code> time.
+	 *
+	 * @param list       The list to merge <code>left</code> and <code>right</code> into.
+	 * @param left       The first array to merge from.
+	 * @param right      The second array to merge from.
+	 * @param comparator The comparator to use when merging.
+	 * @param <T>        The type of the values in the arrays.
+	 */
+	private static <T> void merge(List<T> list, T[] left, T[] right, Comparator<T> comparator)
+	{
+		int size = list.size();
+		int l    = 0;
+		int r    = 0;
+		for (int x = 0; x < size; x++) {
+			if (l == left.length)
+				list.set(x, right[r++]);
+			else if (r == right.length)
+				list.set(x, left[l++]);
+			else
+				list.set(x, comparator.compare(left[l], right[r]) < 1 ? left[l++] : right[r++]);
+		}
+	}
 
 	/**
 	 * Sorts the provided array of integers in ascending order using the quicksort algorithm.
@@ -10,6 +123,9 @@ public class Sorting
 	 */
 	public static void quicksort(int[] array)
 	{
+		if (array.length < 2)
+			return;
+
 		quicksort(array, 0, array.length - 1);
 	}
 
@@ -23,40 +139,36 @@ public class Sorting
 	 */
 	private static void quicksort(int[] array, int left, int right)
 	{
-		int i     = left;
-		int j     = right;
-		int pivot = array[right];
+		int l     = left;
+		int r     = right;
+		int pivot = array[(left + right) / 2];
 
-		while (i <= j) {
-
-			while (array[i] < pivot)
-				i++;
-
-			while (array[j] > pivot)
-				j--;
-
-			if (i <= j)
-				swap(array, i++, j--);
+		while (l < r) {
+			while (array[l] < pivot)
+				l++;
+			while (array[r] > pivot)
+				r--;
+			if (l <= r)
+				swap(array, l++, r--);
 		}
 
-		if (left < j)
-			quicksort(array, left, j);
-
-		if (i < right)
-			quicksort(array, i, right);
+		if (left < r)
+			quicksort(array, left, r);
+		if (right > l)
+			quicksort(array, l, right);
 	}
 
 	/**
-	 * Swaps the integer values at index <code>i</code> and index <code>j</code> in the provided array.
+	 * Swaps the values at index <code>a</code> and <code>b</code> in the provided <code>array</code>.
 	 *
-	 * @param array The array where the values are swapped.
-	 * @param i     The index of the first value to swap.
-	 * @param j     The index of the second value to swap.
+	 * @param array The array to swap values in.
+	 * @param a     The first index.
+	 * @param b     The second index.
 	 */
-	private static void swap(int[] array, int i, int j)
+	private static void swap(int[] array, int a, int b)
 	{
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
+		int temp = array[a];
+		array[a] = array[b];
+		array[b] = temp;
 	}
 }
--- src/test/java/tvestergaard/sorting/SortingTest.java
+++ src/test/java/tvestergaard/sorting/SortingTest.java
@@ -2,30 +2,77 @@ package tvestergaard.sorting;
 
 import org.junit.Test;
 
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertSame;
 
 public class SortingTest
 {
 
-	private int[] random(int size)
+	@Test
+	public void mergesortArray() throws Exception
 	{
-		Random random = new Random();
-		int[]  result = new int[size];
-		for (int x = 0; x < size; x++)
-			result[x] = random.nextInt();
-
-		return result;
+		Integer[] actual   = {0, 5, 8, 3, 4, 1, 2, 7, 9, 6};
+		Integer[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		Sorting.mergesort(actual, Integer::compare);
+		assertArrayEquals(expected, actual);
 	}
 
 	@Test
-	public void quicksort() throws Exception
+	public void mergesortArrayStability() throws Exception
 	{
-		int[] data = random(10000);
-		Sorting.quicksort(data);
-		for (int x = 1; x < 10000; x++) {
-			assertTrue(data[x - 1] < data[x]);
-		}
+		Integer   first    = new Integer(9);
+		Integer   second   = new Integer(9);
+		Integer[] actual   = {7, 4, 5, 8, first, 1, 3, 2, 6, second};
+		Integer[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 9};
+		Sorting.mergesort(actual, Integer::compare);
+		assertArrayEquals(expected, actual);
+		assertSame(first, actual[8]);
+		assertSame(second, actual[9]);
+	}
+
+	@Test
+	public void mergesortList() throws Exception
+	{
+		List<Integer> actual   = toList(0, 5, 8, 3, 4, 1, 2, 7, 9, 6);
+		List<Integer> expected = toList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+		Sorting.mergesort(actual, Integer::compare);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void mergesortListStability() throws Exception
+	{
+		Integer       first  = new Integer(9);
+		Integer       second = new Integer(9);
+		List<Integer> actual = toList(7, 4, 5, 8, -1, 1, 3, 2, 6, -1);
+		actual.set(4, first);
+		actual.set(9, second);
+		List<Integer> expected = toList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9);
+		Sorting.mergesort(actual, Integer::compare);
+		assertEquals(expected, actual);
+		assertSame(first, actual.get(8));
+		assertSame(second, actual.get(9));
+	}
+
+	@Test
+	public void quicksortArray() throws Exception
+	{
+		int[] actual   = {0, 5, 8, 3, 4, 1, 2, 7, 9, 6};
+		int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		Sorting.quicksort(actual);
+		assertArrayEquals(actual, expected);
+	}
+
+	private <T> List<T> toList(T... arguments)
+	{
+		List<T> list = new ArrayList<>();
+		for (T argument : arguments)
+			list.add(argument);
+
+		return list;
 	}
 }
